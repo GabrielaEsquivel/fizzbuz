69529704b5770b4afdbad993f929c0ed63e7b7a7
Andrea <andrea.rueda-ext@dominion-global.com>
2022-04-25 21:17:15 -0500

    Paso 9: Fix unit test y Explorer Service, test now passed

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -1,8 +1,16 @@
 const ExplorerService = require("../../lib/services/ExplorerService")
 describe("Test para ExplorerService", () =>{
     test("Requerimiento 1: Calcular todos los explorers en una missión determinada", ()=>{
-        const explorers = [{mission: 'node'}];
-        const explorersInNode = ExplorerService.filterByMission(explorers, 'node');
+        const explorers = [
+            {
+                "name": "Woopa15",
+                "githubUsername": "ajolonauta15",
+                "score": 15,
+                "mission": "node",
+            }
+        ]
+        const explorersInNode = ExplorerService.filterByMission(explorers, "node");
+        console.log("explorers", explorersInNode)
         expect(explorersInNode.length).toBe(1)
     })
 })
