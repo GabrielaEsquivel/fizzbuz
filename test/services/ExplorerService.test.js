e433d55b737d1186fc1c0edabda4c30c927aaf6b
Andrea <andrea.rueda-ext@dominion-global.com>
2022-04-25 21:04:42 -0500

    Paso 8: Agregando pruebas unitarias

---
+++ test/services/ExplorerService.test.js
@@ -0,0 +1,8 @@
+const ExplorerService = require("../../lib/services/ExplorerService")
+describe("Test para ExplorerService", () =>{
+    test("Requerimiento 1: Calcular todos los explorers en una missión determinada", ()=>{
+        const explorers = [{mission: 'node'}];
+        const explorersInNode = ExplorerService.filterByMission(explorers, 'node');
+        expect(explorersInNode.length).toBe(1)
+    })
+})
