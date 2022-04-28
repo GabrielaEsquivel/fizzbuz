f28845a01a7a131ac1c367ae8414b2b4b2f9ea97
Andrea <andrea.rueda-ext@dominion-global.com>
2022-04-28 18:59:06 -0500

    Paso 19: Agregando funciones a indexjs

--- index.js
+++ index.js
@@ -5,3 +5,6 @@ const explorers = Reader.readJsonFile('../fizzbuz/explorers.json')
 console.log(explorers)
 // Aplicación del ExplorerService en la lista
 console.log(ExplorerService.filterByMission(explorers, 'node'))
+console.log(ExplorerService.getAmountOfExplorersByMission(explorers, 'node'))
+console.log(ExplorerService.getExplorersUsernamesByMission(explorers, 'node'))
+
