2215f83fc0e439b5e7df3584b7f32b8abe98cbd5
Andrea <andrea.rueda-ext@dominion-global.com>
2022-04-28 20:54:30 -0500

    Paso 24: Empezando a agregar routing

--- index.js
+++ index.js
@@ -1,3 +1,40 @@
+// Usando objeto express
+const express = require('express')
+// App de Express
+const app = express()
+app.use(express.json()) // Indicamos que usaremos JSON
+// Puerto en que vamos a ver nuestra app: localhost:3000
+const port = 3000
+
+// Con esto inicializamos esta app
+app.listen(port, () => {
+ console.log(`Example app listening on port ${port}`)
+})
+
+// routes
+
+//HTTP Methods
+app.get('/v1/explorers/:mission', (req, res) => {
+    res.send(`GET METHOD IMPLEMENTED, Explorers V1 API  ${new Date()}`)
+
+    // HTTP STATUS: 200 successful 
+    res.status(200).json(explorers)
+
+})
+
+app.get('/v1/explorers/amount/:mission', (req, res) => {
+    res.send(`GET METHOD IMPLEMENTED, Explorers V1 API  ${new Date()}`)
+
+    // HTTP STATUS: 200 successful 
+    res.status(200).json(explorer)
+
+})
+
+
+
+
+
+
 const Reader = require("./lib/utils/Reader");
 const ExplorerService = require("./lib/services/ExplorerService");
 
