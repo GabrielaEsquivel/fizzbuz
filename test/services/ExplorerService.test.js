8c73067124c73500955729c791263ffb2341e66b
Andrea <andrea.rueda-ext@dominion-global.com>
2022-04-28 19:30:08 -0500

    Paso 21: Agregando pruebas para ExplorerService functions

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -1,16 +1,26 @@
 const ExplorerService = require("../../lib/services/ExplorerService");
+const explorers = [
+    {
+        "name": "Woopa15",
+        "githubUsername": "ajolonauta15",
+        "score": 15,
+        "mission": "node",
+    }
+];
 describe("Test para ExplorerService", () =>{
     test("Requerimiento 1: Calcular todos los explorers en una missión determinada", ()=>{
-        const explorers = [
-            {
-                "name": "Woopa15",
-                "githubUsername": "ajolonauta15",
-                "score": 15,
-                "mission": "node",
-            }
-        ];
         const explorersInNode = ExplorerService.filterByMission(explorers, "node");
         console.log("explorers", explorersInNode);
         expect(explorersInNode.length).toBe(1);
-    });
+    }),
+    test("Requerimiento 2: Obtener el total de explorers en una missión determinada", ()=>{
+        const amountOfExplorersInNode = ExplorerService.getAmountOfExplorersByMission(explorers, "node");
+        console.log("explorers", amountOfExplorersInNode);
+        expect(amountOfExplorersInNode).toBe(1);
+    }),
+    test("Requerimiento 3: Obtener los usernames de los explorers en una missión determinada", ()=>{
+        const explorersUsernames = ExplorerService.getExplorersUsernamesByMission(explorers, "node");
+        console.log("explorers", explorersUsernames);
+        expect(explorersUsernames[0]).toBe('ajolonauta15');
+    })
 });
