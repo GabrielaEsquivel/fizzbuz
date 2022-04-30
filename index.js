9375e223e628c6939bee25f2843f20406b2da79b
Andrea <andrea.rueda-ext@dominion-global.com>
2022-04-29 20:07:26 -0500

    Paso 25: Agregando rutas para explorers in a mission y amount of explorers in a mission

--- index.js
+++ index.js
@@ -1,47 +1,61 @@
-// Usando objeto express
-const express = require('express')
-// App de Express
-const app = express()
+// [EXPRESS] object
+const express = require('express');
+
+// [EXPRESS] app
+const app = express();
 app.use(express.json()) // Indicamos que usaremos JSON
-// Puerto en que vamos a ver nuestra app: localhost:3000
+
+// [PORT] declared
 const port = 3000
 
 // Con esto inicializamos esta app
 app.listen(port, () => {
- console.log(`Example app listening on port ${port}`)
+    console.log(`App listening on port ${port}`)
 })
 
-// routes
+// [SERVICE] imported 
+const ExplorerService = require("./lib/services/ExplorerService");
+
+// [READER UTIL] imported
+const Reader = require("./lib/utils/Reader");
+
+
+// [EXPLORERS LIST] imported
+const explorers = Reader.readJsonFile("../fizzbuz/explorers.json");
+
+// [ROUTES] implemented
 
 //HTTP Methods
 app.get('/v1/explorers/:mission', (req, res) => {
-    res.send(`GET METHOD IMPLEMENTED, Explorers V1 API  ${new Date()}`)
+    //Obtener lista de explorers que están en la misión especificada º
 
+    const { mission }= req.params;
+    const explorersInTheList = ExplorerService.filterByMission(explorers, mission);
     // HTTP STATUS: 200 successful 
-    res.status(200).json(explorers)
+    res.status(200).json(explorersInTheList);
 
-})
+});
 
 app.get('/v1/explorers/amount/:mission', (req, res) => {
-    res.send(`GET METHOD IMPLEMENTED, Explorers V1 API  ${new Date()}`)
-
+    // Obtener cantidad de explorers en una misión
+    const { mission }= req.params;
+    
+    const amountOfExplorers = ExplorerService.getAmountOfExplorersByMission(explorers, mission);
     // HTTP STATUS: 200 successful 
-    res.status(200).json(explorer)
+    res.status(200).json(amountOfExplorers)
 
-})
-
-
+});
 
 
 
 
-const Reader = require("./lib/utils/Reader");
-const ExplorerService = require("./lib/services/ExplorerService");
 
-const explorers = Reader.readJsonFile("../fizzbuz/explorers.json");
+/*
+
 console.log(explorers);
 // Aplicación del ExplorerService en la lista
 console.log(ExplorerService.filterByMission(explorers, "node"));
 console.log(ExplorerService.getAmountOfExplorersByMission(explorers, "node"));
 console.log(ExplorerService.getExplorersUsernamesByMission(explorers, "node"));
+*/
 
